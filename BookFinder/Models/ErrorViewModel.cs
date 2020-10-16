a1f8aecd84fe343813088cb5ee271993e2c78411
kishankamlesh <38970900+kishankamlesh@users.noreply.github.com>
2020-10-16 14:36:21 +0530

    Add project files.

---
+++ BookFinder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookFinder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
