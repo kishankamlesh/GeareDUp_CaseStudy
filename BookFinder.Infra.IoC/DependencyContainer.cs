3d64ba48c47af9f47eafc320d2efb315fd8cf806
kishankamlesh <38970900+kishankamlesh@users.noreply.github.com>
2020-10-19 12:47:25 +0530

    Introduced depedency injection for Book class

---
+++ BookFinder.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using BookFinder.Apllication.Interfaces;
+using BookFinder.Apllication.Services;
+using BookFinder.Domain.Interfaces;
+using BookFinder.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookFinder.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application Layer
+            services.AddScoped<IBookService, BookService>();
+
+            //infra.Data layer
+            services.AddScoped<IBookRepository, BookRepository>();
+        }
+    }
+}
--- BookFinder/Startup.cs
+++ BookFinder/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BookFinder.Infra.Data.Context;
+using BookFinder.Infra.IoC;
 
 namespace BookFinder
 {
@@ -38,6 +39,8 @@ namespace BookFinder
                 Configuration.GetConnectionString("BookDBConnection")));
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -69,6 +72,10 @@ namespace BookFinder
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+        }
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
