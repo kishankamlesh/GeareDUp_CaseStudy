fd15f78dec262263cec8aab532f9d4a19c052c4e
kishankamlesh <38970900+kishankamlesh@users.noreply.github.com>
2020-10-19 08:33:38 +0530

    Improved Architecture and created the book model.

---
+++ BookFinder.Domain/Models/Books.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookFinder.Domain.Models
+{
+    public class Books
+    {
+        public string isbn { get; set; }
+        public string name { get; set; }
+        public string author { get; set; }
+        public string publisher { get; set; }
+        public float cost { get; set; }
+        public string descriptuon { get; set; }
+    }
+}
