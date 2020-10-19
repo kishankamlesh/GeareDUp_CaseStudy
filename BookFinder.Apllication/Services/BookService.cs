0f46b7a5f714853178e04d6c8ab91c2fa8ae95ec
kishankamlesh <38970900+kishankamlesh@users.noreply.github.com>
2020-10-19 12:13:38 +0530

    BookService interface implemented

---
+++ BookFinder.Apllication/Services/BookService.cs
@@ -0,0 +1,23 @@
+﻿using BookFinder.Apllication.Interfaces;
+using BookFinder.Apllication.ViewModels;
+using BookFinder.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookFinder.Apllication.Services
+{
+    public class BookService : IBookService
+    {
+        private IBookRepository _bookRepository;
+
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        public IEnumerable<BookViewModel> GetBooks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
