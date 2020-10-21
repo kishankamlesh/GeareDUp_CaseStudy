d3f84257df44838ad49419659105a9b800a8789b
kishankamlesh <38970900+kishankamlesh@users.noreply.github.com>
2020-10-21 08:36:58 +0530

    Apllied View all logic and UI, using controllers and view models.

---
+++ BookFinder/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookFinder.Apllication.Interfaces;
+using BookFinder.Apllication.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookFinder.Controllers
+{
+    public class BookController : Controller
+    {
+        private IBookService _bookService;
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+        public IActionResult Index()
+        {
+            BookViewModel model = _bookService.GetBooks();
+            return View();
+        }
+    }
+}
