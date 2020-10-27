b8ffdf514a396146e5b16e0ea9d702e32c708dd1
kishankamlesh <38970900+kishankamlesh@users.noreply.github.com>
2020-10-27 17:40:11 +0530

    Added More unit tests for almost 100% coverage.

--- BookFinder.UnitTest/RepositoryTest.cs
+++ BookFinder.UnitTest/RepositoryTest.cs
@@ -1,16 +1,26 @@
 using BookFinder.Domain.Models;
 using BookFinder.Infra.Data.Context;
 using BookFinder.Infra.Data.Repository;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BookFinder.UnitTest
 {
     [TestClass]
     public class RepositoryTest
     {
-        private BookDBContext _ctx;
-        public void BookRepository(BookDBContext ctx)
+        public RepositoryTest()
         {
+            InitContext();
+        }
+        private BookDBContext _ctx;
+        public void InitContext()
+        {
+            var builder = new DbContextOptionsBuilder<BookDBContext>().UseSqlServer("Server=G3;Database=BookDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+            var ctx = new BookDBContext(builder.Options);
             _ctx = ctx;
         }
         [TestMethod]
@@ -32,6 +42,62 @@ namespace BookFinder.UnitTest
             var result = testBook.AddBook(new Book { isbn = "9781234567025", name = "Demo", author = "Demo", publisher = "Demo", cost = 10, descriptuon = "Demo" }); //adding an ISBN which already exists
             //Assert
             Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GetBooks_ReturnsList()
+        {
+            //Arrange
+            var testBook = new BookRepository(_ctx);
+
+            //Act
+            var result = testBook.GetBooks();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<Book>));
+        }
+        [TestMethod]
+        public void GetSearchResults_ReturnsBook()
+        {
+            //Arrange
+            var testBook = new BookRepository(_ctx);
+            string keyword = "Harry";
+            //Act
+            var result = testBook.GetSearchResult(keyword);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(List<Book>));
+        }
+        [TestMethod]
+        public void GetSearchResults_ReturnsListOfBooks()
+        {
+            //Arrange
+            var testBook = new BookRepository(_ctx);
+            string keyword = "The";
+            //Act
+            var result = testBook.GetSearchResult(keyword);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(List<Book>));
+        }
+        [TestMethod]
+        public void GetSearchResults_ReturnsEmptyList()
+        {
+            //Arrange
+            var testBook = new BookRepository(_ctx);
+            string keyword = "DemoBook";
+            //Act
+            var result = testBook.GetSearchResult(keyword);
+            bool emptyListReturned;
+            if (result.Count == 0)
+            {
+                emptyListReturned = true;
+            }
+            else
+            {
+                emptyListReturned = false;
+            }
+            //Assert
+            Assert.IsTrue(emptyListReturned);
         }
     }
 }
